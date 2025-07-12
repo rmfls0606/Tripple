751c3226e77cf7e2d5dec21cbf59bd1b46a4e188
rmfls0606 <rmfls0606@naver.com>
2025-07-13 05:14:48 +0900

    TravelDetailInfoAdTableViewCell 생성 후 storyboard와 연결, IBOutlet 선언

---
+++ Tripple/View/TravelDetailInfoAdTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TravelDetailInfoADTableViewCell.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/13/25.
+//
+
+import UIKit
+
+class TravelDetailInfoAdTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var adBackgroundView: UIView!
+    @IBOutlet weak var adTextLabel: UILabel!
+    @IBOutlet weak var adTagLabel: UILabel!
+}
