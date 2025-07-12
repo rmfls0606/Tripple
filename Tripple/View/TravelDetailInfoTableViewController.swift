b3a45219e0fa5a4f230b5c111ee23909e22d3ebd
rmfls0606 <rmfls0606@naver.com>
2025-07-12 19:35:17 +0900

    여행 도시 화면 테이블 셀 생성 코드 작성

--- Tripple/View/TravelDetailInfoTableViewController.swift
+++ Tripple/View/TravelDetailInfoTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class TravelDetailInfoTableViewController: UITableViewController {
 
+    private let travelInfoData: [Travel] = TravelInfo().travel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,5 +20,21 @@ class TravelDetailInfoTableViewController: UITableViewController {
     func configure(){
         self.navigationItem.title = "도시 상세 정보"
         self.navigationItem.largeTitleDisplayMode = .inline
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return travelInfoData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "travelDetailInfoTableViewCell",
+            for: indexPath
+        ) as? TravelDetailInfoTableViewCell else{
+            return UITableViewCell()
+        }
+        
+        
+        return cell
     }
 }
