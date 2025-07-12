be6ba9330285bce11b89cdf8178ffda55b6777a8
rmfls0606 <rmfls0606@naver.com>
2025-07-12 13:34:58 +0900

    Game369ViewController파일 생성 후 storyboard와 연결

---
+++ Tripple/View/Game369ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Game369ViewController.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/12/25.
+//
+
+import UIKit
+
+class Game369ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
