8f9a701c5b3c8cf14431c1dfd5837531ce68d926
rmfls0606 <rmfls0606@naver.com>
2025-07-15 01:05:55 +0900

    self-sizing을 확인 해보기 위해 모델 데이터 수정

--- Tripple/Model/MagazineInfo.swift
+++ Tripple/Model/MagazineInfo.swift
@@ -17,7 +17,7 @@ struct Magazine{
 
 struct MagazineInfo {
     let magazine: [Magazine] = [
-        Magazine(title: "유럽 여행 쇼핑 성지, OOO은 꼭 가세요!", subtitle: "유럽의 인기 쇼핑 명소 총정리", photo_image: "https://cdn.pixabay.com/photo/2016/11/22/21/57/apparel-1850804_1280.jpg", date: "241118", link: "https://triple.guide/articles/265bd919-3f75-4adc-8d5d-c5cf60201bfe"),
+        Magazine(title: "유럽 여행 쇼핑 성지, OOO은 꼭 가세요! 절대 후회하지 않을거예요. 단 한번의 추억을 정말 소중하고 행복한 기억으로 남기길 바랄게요!", subtitle: "유럽의 인기 쇼핑 명소 총정리", photo_image: "https://cdn.pixabay.com/photo/2016/11/22/21/57/apparel-1850804_1280.jpg", date: "241118", link: "https://triple.guide/articles/265bd919-3f75-4adc-8d5d-c5cf60201bfe"),
         Magazine(title: "현지 MZ가 알려주는 오사카 MZ 인기 스팟", subtitle: "2025 오사카 여행엔 여기!", photo_image: "https://cdn.pixabay.com/photo/2020/10/22/14/05/moon-5676124_1280.jpg", date: "250103", link: "https://triple.guide/articles/d6c5257f-4d52-4a0a-aed2-6773961ee7be"),
         Magazine(title: "2025 NEWS 해외여행, 이렇게 달라졌다!", subtitle: "알고 준비하는 2025 새 여행", photo_image: "https://cdn.pixabay.com/photo/2020/03/31/10/42/park-4987140_1280.jpg", date: "250108", link: "https://triple.guide/articles/997d84b1-25b3-4494-a400-457d1205264a"),
         Magazine(title: "소중한 연차, 이때 쓰세요! 2025 황금연휴 캘린더", subtitle: "미리 세우는 2025년 여행 계획", photo_image: "https://cdn.pixabay.com/photo/2014/02/24/18/31/meeting-273857_1280.jpg", date: "241225", link: "https://triple.guide/articles/b373b73a-f7e1-4202-932b-0d8cb188042a"),
@@ -30,7 +30,7 @@ struct MagazineInfo {
         Magazine(title: "크리스마스 분위기 가득! 국내 일루미네이션 명소 ✨", subtitle: "요즘 SNS에서 핫한 장소 총정리", photo_image: "https://cdn.pixabay.com/photo/2014/04/10/15/37/snowman-321034_1280.jpg", date: "250101", link: "https://triple.guide/content/articles/ca81200f-e367-4fe8-988f-2fa8d0b5c421"),
         Magazine(title: "야시장 천국 '대만 여행' 먹킷리스트", subtitle: "요즘 가장 인기 있는 메뉴는 OO", photo_image: "https://cdn.pixabay.com/photo/2017/06/11/17/03/dumplings-2392893_1280.jpg", date: "250103", link: "https://triple.guide/content/articles/69f887b0-4b87-451f-8859-82c7461858b9"),
         Magazine(title: "핑크빛 바다가 펼쳐지는 환상의 섬, 롬복", subtitle: "잊히지 않는 TV속 그곳!", photo_image: "https://cdn.pixabay.com/photo/2019/08/17/19/13/landscape-4412784_1280.jpg", date: "250101", link: "https://triple.guide/content/articles/8c4cded5-ff1e-4898-b884-c7b9f84fd5d8"),
-        Magazine(title: "여행 고수가 알려주는 새벽 비행기 이용 꿀팁", subtitle: "이제 공항에서 시간 때울 걱정 끝!", photo_image: "https://cdn.pixabay.com/photo/2021/09/07/11/53/car-6603726_1280.jpg", date: "250105", link: "https://triple.guide/content/articles/791578f3-95d7-4300-a51f-6dff48db5d55"),
+        Magazine(title: "여행 고수가 알려주는 새벽 비행기 이용 꿀팁", subtitle: "이제 공항에서 시간 때울 걱정 끝! 여행 고수가 알려주는 새벽 비행기 이용 꿀팁을 이용하여, 편하고 안전한 여행을 시작해 보세요!", photo_image: "https://cdn.pixabay.com/photo/2021/09/07/11/53/car-6603726_1280.jpg", date: "250105", link: "https://triple.guide/content/articles/791578f3-95d7-4300-a51f-6dff48db5d55"),
 
     ]
 }
