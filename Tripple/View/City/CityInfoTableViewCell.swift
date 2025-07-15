cfbee60be4b7db9af39f448932befb39c400b204
rmfls0606 <rmfls0606@naver.com>
2025-07-16 02:15:21 +0900

    인기 도시 화면에서 Xib로 커스텀 셀 구성 및 AutoLayout 설정, CityInfoTableViewCell과 연결

---
+++ Tripple/View/City/CityInfoTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CityInfoTableViewCell.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/16/25.
+//
+
+import UIKit
+
+class CityInfoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityImageView: UIImageView!
+    @IBOutlet weak var cityExplainLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
