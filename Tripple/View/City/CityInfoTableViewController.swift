931bb6abe4fa5ab0872bd5548d1be2d2cc61210f
rmfls0606 <rmfls0606@naver.com>
2025-07-16 02:42:58 +0900

    인기 도시 화면 Xib파일의 커스텀 셀을 사용하기 위해 CityInfoTableViewController에 tableView.register() 설정

--- Tripple/View/City/CityInfoTableViewController.swift
+++ Tripple/View/City/CityInfoTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CityInfoTableViewController: UITableViewController {
 
+    let cityInfoData: [City] = CityInfo().city
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,5 +20,24 @@ class CityInfoTableViewController: UITableViewController {
     func configure(){
         navigationItem.title = "인기 도시"
         navigationItem.largeTitleDisplayMode = .inline
+        
+        let xib = UINib(nibName: CityInfoTableViewCell.identifier, bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: CityInfoTableViewCell.identifier)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cityInfoData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CityInfoTableViewCell.identifier, for: indexPath) as? CityInfoTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 }
