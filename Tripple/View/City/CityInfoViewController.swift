8a6bd7163ee34fd7b5595e4beef269851978cc4c
rmfls0606 <rmfls0606@naver.com>
2025-07-16 20:13:43 +0900

    CityInfoViewController 만든 후 storyboard와 연결 및 IBOutlet 연결

---
+++ Tripple/View/City/CityInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CityInfoViewController.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/16/25.
+//
+
+import UIKit
+
+class CityInfoViewController: UIViewController {
+
+    @IBOutlet var cityInputTextField: UITextField!
+    @IBOutlet var cityInfoTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
