0c5734d2ab5226f1c003ddddb2a9bce59ba4addd
rmfls0606 <rmfls0606@naver.com>
2025-07-16 19:23:46 +0900

    CityInfoDetailViewController 만든 후 storyboard와 연결 및 IBOutlet연결

---
+++ Tripple/View/City/CityInfoDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CityInfoDetailViewController.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/16/25.
+//
+
+import UIKit
+
+class CityInfoDetailViewController: UIViewController {
+
+    @IBOutlet weak var cityImageView: UIImageView!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityEnglishNameLabel: UILabel!
+    @IBOutlet weak var cityCountryLabel: UILabel!
+    @IBOutlet weak var cityExplainLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
