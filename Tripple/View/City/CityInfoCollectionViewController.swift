584623a8c769944e91c6b3b6ba073e5199f5fc3f
rmfls0606 <rmfls0606@naver.com>
2025-07-17 16:27:25 +0900

    인기 도시 화면(collectionView) 커스텀 셀 파일과 연결 및 컬렉션 셀 생성

--- Tripple/View/City/CityInfoCollectionViewController.swift
+++ Tripple/View/City/CityInfoCollectionViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class CityInfoCollectionViewController: UIViewController {
+class CityInfoCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet var cityInputTextField: UITextField!
     @IBOutlet var cityInfoCollectionView: UICollectionView!
+    
+    private var cityInfoData = CityInfo().city
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,5 +25,42 @@ class CityInfoCollectionViewController: UIViewController {
         navigationItem.largeTitleDisplayMode = .inline
         
         cityInputTextField.placeholder = "도시를 입력해주세요..."
+        
+        cityInfoCollectionView.delegate = self
+        cityInfoCollectionView.dataSource = self
+        
+        let xib = UINib(nibName: "CityInfoCollectionViewCell", bundle: nil)
+        cityInfoCollectionView.register(xib, forCellWithReuseIdentifier: "CityInfoCollectionViewCell")
+        
+        let layout = UICollectionViewFlowLayout()
+        let padding = 16.0
+        let spacing = 8.0
+        let dimension = (UIScreen.main.bounds.width - (padding * 2) - spacing) / 2
+        layout.itemSize = CGSize(width: dimension, height: dimension * 1.4)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.sectionInset = UIEdgeInsets(
+            top: 0,
+            left: padding,
+            bottom: spacing,
+            right: padding
+        )
+        
+        cityInfoCollectionView.collectionViewLayout = layout
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cityInfoData.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "CityInfoCollectionViewCell",
+            for: indexPath
+        ) as? CityInfoCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
 }
