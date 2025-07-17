f57306e6070c1f6592017881642d81e895e35271
rmfls0606 <rmfls0606@naver.com>
2025-07-17 15:01:06 +0900

    인기 도시 화면(collectionView) 커스텀 셀 xib 파일로 구성 및 AutoLayout 설정

---
+++ Tripple/View/City/CityInfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CityInfoCollectionViewCell.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/17/25.
+//
+
+import UIKit
+
+class CityInfoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
