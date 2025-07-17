d5fc22bea3651a97ff0e8a100a67e9af415f351a
rmfls0606 <rmfls0606@naver.com>
2025-07-17 16:43:17 +0900

    인기 도시 화면 컬렉션 뷰 셀 Xib파일과 IBOutlet연결 및 스타일 지정, 셀에 데이터 반영하기

--- Tripple/View/City/CityInfoCollectionViewCell.swift
+++ Tripple/View/City/CityInfoCollectionViewCell.swift
@@ -17,14 +17,16 @@ class CityInfoCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         cityImageView.contentMode = .scaleAspectFill
-//        cityImageView.backgroundColor = .systemGray6
+        cityImageView.backgroundColor = .systemGray6
         
         cityNameLabel.font = .boldSystemFont(ofSize: 16)
         cityNameLabel.numberOfLines = 2
+        cityNameLabel.textAlignment = .center
         
         cityExplainLabel.font = .systemFont(ofSize: 14)
         cityExplainLabel.textColor = .systemGray2
         cityExplainLabel.numberOfLines = 2
+        cityExplainLabel.textAlignment = .center
     }
 
     func configure(city: City){
