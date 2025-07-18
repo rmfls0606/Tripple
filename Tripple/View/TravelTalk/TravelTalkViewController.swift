dcbd72de10be0353376136c90a7673635b4bbdc6
rmfls0606 <rmfls0606@naver.com>
2025-07-19 00:44:23 +0900

    Travel Talk ViewController와 storyboard 연결 및 IBOutlet 연결

---
+++ Tripple/View/TravelTalk/TravelTalkViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelTalkViewController.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/19/25.
+//
+
+import UIKit
+
+class TravelTalkViewController: UIViewController {
+
+
+    @IBOutlet var travelTalkSearchBar: UISearchBar!
+    @IBOutlet var travelTalkCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
