87e1779ef509ff32553d728ac5b7b1c27050c2a2
rmfls0606 <rmfls0606@naver.com>
2025-07-20 20:26:46 +0900

    나의 채팅을 보여주는 Xib 파일의 IBOutlet 연결 및 스타일 설정

--- Tripple/View/TravelTalk/MeTableViewCell.swift
+++ Tripple/View/TravelTalk/MeTableViewCell.swift
@@ -8,16 +8,35 @@
 import UIKit
 
 class MeTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    @IBOutlet var contentBorderView: UIView!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    static let identifier = "MeTableViewCell"
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentBorderView.layer.cornerRadius = contentBorderView.bounds.height / 4
     }
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        configureView()
+    }
+    
+    private func configureView(){
+        contentBorderView.layer.borderWidth = 1.0
+        contentBorderView.layer.borderColor = UIColor.systemGray.cgColor
+        contentBorderView.backgroundColor = .systemGray4
+        
+        contentLabel.font = .systemFont(ofSize: 12)
+        contentLabel.numberOfLines = 0
+        
+        dateLabel.font = .systemFont(ofSize: 10)
+        dateLabel.textColor = .systemGray
+        
+        selectionStyle = .none
+    }
 }
