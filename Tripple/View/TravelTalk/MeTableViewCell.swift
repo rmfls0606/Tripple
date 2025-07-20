d31ed81dd2a4d4f5e7948da23afd1555519ba4a6
rmfls0606 <rmfls0606@naver.com>
2025-07-20 20:22:02 +0900

    나의 채팅을 보여주는 커스텀 테이블 셀 storyboard 구성 및 AutoLayout설정

---
+++ Tripple/View/TravelTalk/MeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MeTableViewCell.swift
+//  Tripple
+//
+//  Created by 이상민 on 7/20/25.
+//
+
+import UIKit
+
+class MeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
